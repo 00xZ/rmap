cae5f59514782668d8411ecad241c9afcd3db420
Eyezik <eyezikbra@gmail.com>
2021-11-02 21:46:33 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
+
 extern crate rand;
 
 use rand::thread_rng;
 use rand::Rng;
 
 fn main() {
-    let mut rng = thread_rng();
-    let distr = rand::distributions::Uniform::new_inclusive(1, 100);
-    let mut nums = [0i32; 3];
-    for x in &mut nums {
-        *x = rng.sample(distr);
-    }
-    println!("Some numbers: {:?}", nums);
+        let a: i64 = rand::thread_rng().gen_range(0..255);
+        let b: i64 = rand::thread_rng().gen_range(0..255);
+        let c: i64 = rand::thread_rng().gen_range(0..255);
+        let d: i64 = rand::thread_rng().gen_range(0..255);
+        let ip = format!("{}.{}.{}.{}", a,b,c,d);
+        println!("IP: {}", ip);
 }
