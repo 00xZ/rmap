e86f87f28c16c8ba8e882f8472fc715434511827
Eyezik <eyezikbra@gmail.com>
2021-11-03 21:33:36 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
-
 extern crate rand;
-
+extern crate port_scanner;
+use port_scanner::scan_port_addr;
 use rand::thread_rng;
 use rand::Rng;
-
+fn scan(mut param_target:String) {
+        println!("IP: {}", param_target);
+	    let tester = scan_port_addr(param_target);
+        println!("{}", tester);
+        if tester == false {
+            println!("Closed");
+        }
+        else {
+            println!("OPEN");
+        }
+}
 fn main() {
         let a: i64 = rand::thread_rng().gen_range(0..255);
         let b: i64 = rand::thread_rng().gen_range(0..255);
         let c: i64 = rand::thread_rng().gen_range(0..255);
         let d: i64 = rand::thread_rng().gen_range(0..255);
-        let ip = format!("{}.{}.{}.{}", a,b,c,d);
-        println!("IP: {}", ip);
+        let target = format!("{}.{}.{}.{}:80", a,b,c,d);
+        //println!("IP: {}", target);
+        scan(target);
 }
