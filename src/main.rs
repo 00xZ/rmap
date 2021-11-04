fce93a4bc416356b8ba0cd2ee571ce93c1edc054
Eyezik <eyezikbra@gmail.com>
2021-11-03 22:14:27 -0700

    Update main.rs
    It scans ips, and puts open ones in output, now to add multi threading and show the real power of rust

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,27 @@
+
 extern crate rand;
 extern crate port_scanner;
 use port_scanner::scan_port_addr;
-use rand::thread_rng;
 use rand::Rng;
-fn scan(mut param_target:String) {
+use std::borrow::Borrow;
+use std::fs::OpenOptions;
+use std::io::Write;
+use std::fs::File;
+fn scan(param_target:String) {
+	    File::create("list.txt");
+        let mut file = OpenOptions::new().append(true).open("list.txt").expect("Cant open file check permissions.");
         println!("IP: {}", param_target);
-	    let tester = scan_port_addr(param_target);
+	    let tester = scan_port_addr(&param_target);
         println!("{}", tester);
         if tester == false {
             println!("Closed");
         }
         else {
             println!("OPEN");
+            file.write(param_target.as_bytes());
         }
 }
-fn main() {
+fn genip() {
         let a: i64 = rand::thread_rng().gen_range(0..255);
         let b: i64 = rand::thread_rng().gen_range(0..255);
         let c: i64 = rand::thread_rng().gen_range(0..255);
@@ -22,4 +29,14 @@ fn main() {
         let target = format!("{}.{}.{}.{}:80", a,b,c,d);
         //println!("IP: {}", target);
         scan(target);
+}
+fn main() {
+	let mut count = 0u32;
+	loop {
+		count += 1;
+        genip();
+        if count == 10 {
+			break;
+        }
+	}
 }
