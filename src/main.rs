a3517137611db9b94118670517d94cf2e1f4f9c1
Eyezik <eyezikbra@gmail.com>
2021-11-08 11:53:20 -0800

    Update main.rs
    kinda works

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 
+
 extern crate rand;
 extern crate port_scanner;
 use port_scanner::scan_port_addr;
@@ -6,13 +7,13 @@ use rand::Rng;
 use std::borrow::Borrow;
 use std::fs::OpenOptions;
 use std::io::Write;
+use std::thread;
+use std::time::Duration;
 use std::fs::File;
 fn scan(param_target:String) {
-	    File::create("list.txt");
         let mut file = OpenOptions::new().append(true).open("list.txt").expect("Cant open file check permissions.");
         println!("IP: {}", param_target);
 	    let tester = scan_port_addr(&param_target);
-        println!("{}", tester);
         if tester == false {
             println!("Closed");
         }
@@ -21,22 +22,19 @@ fn scan(param_target:String) {
             file.write(param_target.as_bytes());
         }
 }
-fn genip() {
-        let a: i64 = rand::thread_rng().gen_range(0..255);
-        let b: i64 = rand::thread_rng().gen_range(0..255);
-        let c: i64 = rand::thread_rng().gen_range(0..255);
-        let d: i64 = rand::thread_rng().gen_range(0..255);
-        let target = format!("{}.{}.{}.{}:80", a,b,c,d);
-        //println!("IP: {}", target);
-        scan(target);
-}
+
 fn main() {
 	let mut count = 0u32;
 	loop {
 		count += 1;
-        genip();
-        if count == 10 {
-			break;
-        }
-	}
+		let a: i64 = rand::thread_rng().gen_range(0..255);
+        let b: i64 = rand::thread_rng().gen_range(0..255);
+        let c: i64 = rand::thread_rng().gen_range(0..255);
+        let d: i64 = rand::thread_rng().gen_range(0..255);
+        let target = format!("{}.{}.{}.{}:80", a,b,c,d);
+        thread::spawn(|| { scan(target); });
+        thread::sleep(Duration::from_millis(5));
+    if count == 10 {break;}
+    }
+	
 }
